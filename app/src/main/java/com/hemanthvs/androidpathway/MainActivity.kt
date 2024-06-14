fa00efd973ab71c3a2bec0852e7601bc10734ac1
hemanth-vs <s.hemanthvarma@gmail.com>
2024-06-14 15:10:27 -0700

    android/happy-birthday-app: build a simple happy birthday android app

--- app/src/main/java/com/hemanthvs/androidpathway/MainActivity.kt
+++ app/src/main/java/com/hemanthvs/androidpathway/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hemanthvs.androidpathway.ui.theme.AndroidPathwayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidPathwayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday Hinata!",
+                        from = "From Naruto",
+                        modifier = Modifier.padding(8.dp)
                     )
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = message,
+            fontSize = 64.sp,
+            lineHeight = 72.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     AndroidPathwayTheme {
-        Greeting("Android")
+            GreetingText(
+                message = "Happy Birthday Hinata!",
+                from = "From Naruto",
+                modifier = Modifier.padding(8.dp)
+            )
     }
 }
