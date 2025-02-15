a3ea5ff8d0a6462a3718d004920ee6eb5def90c7
hemanth-vs <s.hemanthvarma@gmail.com>
2025-02-15 15:47:06 -0700

    android/business-card: complete app minus final alignment adjustments

--- app/src/main/java/com/hemanthvs/androidpathway/MainActivity.kt
+++ app/src/main/java/com/hemanthvs/androidpathway/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MailOutline
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hemanthvs.androidpathway.ui.theme.AndroidPathwayTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +34,11 @@ class MainActivity : ComponentActivity() {
             AndroidPathwayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        fullName = "Self",
+                        title = "Student",
+                        phone = "Random Phone number",
+                        linkedIn = "Random LinkedIn",
+                        email = "Random email",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +48,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(fullName: String, title: String, phone: String, linkedIn: String, email: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(1f)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(3f).padding(top = 32.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.android_logo),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp)
+                )
+            Text(
+                text = fullName,
+                modifier = Modifier
+            )
+            Text(text = title,
+                modifier = Modifier
+            )
+        }
+        Column(verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier.weight(1f).padding(48.dp)
+            ){
+            Row {
+                Icon(
+                    Icons.Rounded.Phone,
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 16.dp)
+                )
+                Text(text = phone)
+            }
+            Row {
+                Box() {
+                    Image(
+                        painter = painterResource(id = R.drawable.inbug_black),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )}
+                Text(text = linkedIn)
+
+            }
+            Row {
+                Icon(
+                    Icons.Rounded.MailOutline,
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 16.dp)
+                )
+                Text(text = email)
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidPathwayTheme {
-        Greeting("Android")
+//        Greeting("Android")
     }
 }
